c04764c1665f7cce62b840c177cc75ac05be693b
JsteReubsSoftware <joostereuben0830@gmail.com>
2024-04-24 14:35:22 +0200

    Added OAuth2.0 login

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import { GoogleLogin } from '@react-oauth/google';
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import { jwtDecode } from "jwt-decode";
+
 import './index';
 
 function App() {
   return (
     <div className="App">
+      <GoogleOAuthProvider clientId={process.env.REACT_APP_OAUTH_CLIENT_ID}>
+        <GoogleLogin
+          clientId={process.env.REACT_APP_OAUTH_CLIENT_ID}
+          onSuccess={credentialResponse => {
+            const decoded = jwtDecode(credentialResponse?.credential);
+            console.log(decoded);
+          }}
+          onError={() => {
+            console.log('Login Failed');
+          }}
+          useOneTap
+        />
+      </GoogleOAuthProvider>
       <h1>Hello World</h1>
     </div>
   );
